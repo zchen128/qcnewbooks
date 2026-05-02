83a608276a6ff7f8fc5b5987e4c4c8077fad0cc2
zchen128 <zexin.chen61@qmail.cuny.edu>
2026-05-02 18:07:20 -0400

    finished 3.8 - balance sheet activity

---
+++ src/routes/balance/+page.svelte
@@ -0,0 +1,30 @@
+<div class="max-w-2xl mx-auto mt-10 p-8 bg-gray-950 border border-gray-700 rounded-1g">
+	<h1 class="text-2x1 font-bold  text-center mb-6 text-yellow-400">Balance Sheet</h1>
+	<p class="text-center text-gray-500 text-sm mb-6">As of December 31</p>
+	    <h2 class="text-sm font-bold uppercase tracking-wide text-yellow-300 mb-2">Assets</h2>
+	<div class="flex justify-between py-3 border-b border-gray-700">
+		<span class="text-gray-300">Cash</span>
+		<span class="text-green-400 font-mono">$50,000</span>
+	</div>
+	<div class="flex justify-between border-b border-gray-700" py-3>
+		<span class="text-gray-300">Accounts Receivable</span>
+		<span class="font-mono text-green-400">$25,000</span>
+	</div>
+
+	<div class="flex justify-between py-4 px-3 mt-2 bg-yellow-900 rounded">
+		<span class="text-yellow-100 font-bold">Total Assets</span>
+		<span class="text-yellow-300 font-mono font-bold">$75,000</span>
+	</div>
+
+	<h2 class="text-sm font-bold uppercase tracking-wide text-yellow-300 mt-6 mb-2">Liabilities</h2>
+
+	<div class="flex justfy-between py-3 border-b border-gray-700">
+		<span class="text-gray-300">Accounts Payable</span>
+		<span class=" text-red-400 font-mono">$15,000</span>
+	</div>
+
+	<div class="flex justify-between py-4 px-3 mt-4 bg-yellow-900 rounded">
+		<span class="text-yellow-100 font-bold">Owner's Equity</span>
+		<span class="text-yellow-300 font-mono font-bold">$60,000</span>
+	</div>
+</div>
